d2f6efaee8e078a0f5d61c3b85b56506d4a74d06
Johovany Umanzor <me@johovany.dev>
2025-03-05 20:56:35 -0600

    updates for models.py for better transaction/expense logic

--- expense_tracker_django/expenses/models.py
+++ expense_tracker_django/expenses/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
-class Expense(models.Model):
-    date = models.Field.unique_for_date
-    amount = models.DecimalField(max_digits = 10, decimal_places = 2)
-    description = models.CharField(max_length = 10)
-    category = models.Field.choices
-        
+class User(AbstractUser):
+    pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name_plurar = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    TRANSACTION_CATEGORY_CHOICES = (
+        ("income", "Income"),
+        ("expense", "Expense"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=7, choices=TRANSACTION_CATEGORY_CHOICES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.type} of {self.amount} on {self.date} by {self.user}"
